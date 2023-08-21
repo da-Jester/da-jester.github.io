a536a50f24073d158429f4d9c6718a4e63898f2a
da-Jester <88332633+da-Jester@users.noreply.github.com>
2023-08-21 18:33:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# da-Jester.github.io
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-qojcj6)
---
+++ script.js
@@ -0,0 +1,76 @@
+//BASIC FUNCTIONS
+function repeat(times, code) {
+  for (i = 0; i < 100; i++) {
+    code()
+  }
+}
+
+//Canvas Scripts:
+const visibleWidth = document.documentElement.clientWidth;
+const visibleHeight = document.documentElement.clientHeight;
+
+
+const canvas = document.querySelector("#crazyCanvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = visibleWidth
+canvas.height = visibleHeight
+
+let canvasLongestSide = canvas.width
+if (canvas.height > canvas.width) {
+  canvasLongestSide = canvas.height
+}
+
+function clearCanvas() { //clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function drawCircle(x, y, radius, colour, lineWidth) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI); // arc(centre x, centre y, radius)
+  ctx.strokeStyle = colour
+  ctx.lineWidth = lineWidth //set the width of the line
+  ctx.stroke();
+}
+
+//Main Code
+
+function styles(colour) {
+  //background
+  document.querySelector("#stuff").style.backgroundColor = `hsla(${colour}deg, 50%, 50%, 0.5)`
+  //links
+  let colourIterator = colour + 100
+  document.querySelectorAll("a").forEach(link => {
+    link.style.color = `hsl(${colourIterator}deg, 100%, 50%)`
+    colourIterator += 40
+  })
+}
+//.style.color = `hsl(${colour + 200}deg, 100%, 50%)`
+
+let hue = 0
+let radius = 0
+let startHue = 0
+let startRad = 5
+let bright = 0
+
+function drawTunnel() {
+  //startRad *= 1.1
+  startHue -= 2
+  hue = startHue
+  radius = startRad
+  circles()
+  styles(startHue)
+}
+
+function circles() {
+  bright = 0
+  while (radius < canvasLongestSide / 1.5) {
+    drawCircle(canvas.width / 2, canvas.height / 2, radius, `hsl(${hue}deg, 100%, ${bright}%)`, radius / 4)
+    hue += 20
+    radius *= 1.1
+    bright += 1.7
+  }
+}
+
+//repeat(100, () => circles())
+setInterval(drawTunnel, 1000 / 30)
